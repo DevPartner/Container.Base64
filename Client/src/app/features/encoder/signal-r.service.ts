566c7b24ca0d373f55f22311307afa01135447a7
Konstantin Ivinsky <Konstantin_Ivinsky@epam.com>
2024-09-27 14:54:47 +0200

    Update Basic Auth to use server prompt

--- Client/src/app/features/encoder/signal-r.service.ts
+++ Client/src/app/features/encoder/signal-r.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-//import { HubConnection, HubConnectionBuilder, DefaultHttpClient, LogLevel } from '@microsoft/signalr';
 import * as signalR from '@microsoft/signalr';
 
-import { Config } from "./config";
-import { CustomHttpClient } from "./custom-httpclient";
+import { Config } from "../../core/services/config";
 
 @Injectable({
   providedIn: 'root'
@@ -13,34 +11,13 @@ export class SignalRService {
   private hubConnection: signalR.HubConnection;
   constructor() { }
 
-  public startConnection(token: string): Promise<void> {
+  public startConnection(): Promise<void> {
     var url = this.encodinghubUri();
-
-
-    fetch(`${Config.api}/api/health`, {
-      method: 'GET', 
-      headers: {
-        'Authorization': token
-      }
-    }).then(response => {
-      if (response.ok) {
-        console.log('Authentication successful!');
-        // Handle the authenticated response, load data, etc.
-      } else if (response.status === 401) {
-        console.error('Authentication failed. Incorrect login.');
-      }
-    }).catch(error => {
-      console.error('Error:', error);
-    });
-
-    const defaultHttpClient = new signalR.DefaultHttpClient(signalR.NullLogger.instance);
-    const customHttpClient = new CustomHttpClient(defaultHttpClient, token);
 
     this.hubConnection = new signalR.HubConnectionBuilder()
       //.configureLogging(signalR.LogLevel.Trace)
       .withUrl(url, {
         withCredentials: true,
-        httpClient: customHttpClient,
         transport: signalR.HttpTransportType.LongPolling
       })
       .build();
